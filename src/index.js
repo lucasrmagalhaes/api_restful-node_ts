5abd28b859dd9d4bd99ba7c48e5d240d7b10407a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-04-13 22:12:21 -0300

    Filtro com Query Params

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ app.use(express.json());
 const projects = [];
 
 app.get('/projects', (request, response) => {
-    return response.json(projects);
+    const { title } = request.query;
+
+    const results = title 
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
