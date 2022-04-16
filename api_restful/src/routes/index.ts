2c7beb2ca6344afb8cce1795c99949faf03da582
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-04-16 20:44:57 -0300

    Rota para Listar Itens

--- api_restful/src/routes/index.ts
+++ api_restful/src/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import itemsRouter from './items.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Olá DEV!'});
-});
+routes.use('/items', itemsRouter);
 
 export default routes;
