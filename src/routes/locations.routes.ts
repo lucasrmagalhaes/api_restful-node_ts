cca4b21ea00cbb9239cbb86eddbbff009fd4bcf8
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-04-17 09:59:46 -0300

    Rota Exibir um Location

--- src/routes/locations.routes.ts
+++ src/routes/locations.routes.ts
@@ -71,4 +71,21 @@ locationsRouter.post('/', async (request, response) => {
     });
 });
 
+locationsRouter.get('/:id', async (request, response) => {
+    const { id } = request.params;
+
+    const location = await knex('locations').where('id', id).first();
+
+    if (!location) {
+        return response.status(400).json({ message: 'Location not found.' });
+    }
+
+    const items = await knex('items')
+        .join('location_items', 'items.id', '=', 'location_items.item_id')
+        .where('location_items.location_id', id)
+        .select('items.title');
+
+    return response.json({ location, items });
+});
+
 export default locationsRouter;
